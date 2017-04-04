23e7fa3e81da815308a5122629f42a1b14cb1afb
Martin Hochel <hochelmartin@gmail.com>
2017-04-05 01:09:30 +0200

    feat(wc/counter): add counter implementation

--- src/app/web-components/components/Counter.tsx
+++ src/app/web-components/components/Counter.tsx
@@ -1,10 +1,38 @@
-import {h, Component} from 'skatejs';
+import {h, Component, prop} from 'skatejs';
 
 type Props = {};
 class Counter extends Component<Props> {
   static get is(){ return 'sk-counter'; }
+
+  static get props(){
+    return {
+      count: prop.number(),
+      repeatInterval: prop.number(),
+    };
+  }
+
+  count: number;
+  repeatInterval = 1000;
+
+  private timerID: number;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.timerID = this.tick();
+  }
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    clearInterval(this.timerID);
+  }
   renderCallback() {
-    return <div>Hello world</div>;
+    const {count} = this;
+    return <div>Hello world tick: <b>{count}</b></div>;
+  }
+
+  private tick() {
+    return window.setInterval(() => {
+      this.count = this.count + 1;
+    }, this.repeatInterval);
   }
 
 }
